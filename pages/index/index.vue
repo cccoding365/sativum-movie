d00f3e8870c6a4e6d6538de924267fb5288a0e17
DoubledConG <992196871@qq.com>
2023-12-14 22:13:05 +0800

    feat: add movie search

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,27 +1,129 @@
 <template>
 	<uni-nav-bar statusBar title="蒜苗" />
 	<view class="container">
-
+		<uni-search-bar placeholder="搜索你感兴趣的电影" bgColor="#EEEEEE" @confirm="search" @cancel="onSearchCancel" />
+		<view class="movie-list" :show-scrollbar="false" @refresherrefresh="onRefresherrefresh"
+			:refresher-triggered="refresherTriggered" @scrolltolower="onScroll2Lower">
+			<view class="movie-item" v-for="item in movieList.results" :key="item.id" @tap="onMovieDetail(item.id)">
+				<view class="poster">
+					<image class="image" :src="configs.IMAGE_URL.medium + item.poster_path" mode="aspectFit"
+						lazy-load />
+					<view class="vote-average">
+						<text> {{ item.vote_average.toFixed(1) }} </text>
+					</view>
+					<text class="title"> {{ item.title }} </text>
+				</view>
+			</view>
+		</view>
+		<uni-load-more v-if="loadMoreStatus" :status="loadMoreStatus" />
 	</view>
 </template>
 
 <script lang="ts" setup>
+	import { ref } from 'vue';
+	import configs from '@/configs';
+	import apis from '@/apis';
+	import types from '@/types';
 
+	const movieList = ref<types.MovieList>({
+		results: [],
+		page: 0,
+		total_pages: 0,
+		total_results: 0
+	});
+	const loadMoreStatus = ref<types.LoadMoreStatus | null>(null);
+
+	const search = async (e : { value : string; }) => {
+		loadMoreStatus.value = 'loading';
+		const { page: currentPage, results, total_pages } = (await apis.searchMovies(e.value, 1)) as types.MovieList;
+		movieList.value.page = currentPage;
+		movieList.value.results = results;
+		loadMoreStatus.value = currentPage === total_pages ? 'no-more' : 'more';
+	};
+
+	const onSearchCancel = () => {
+		Object.assign(movieList.value, {
+			results: [],
+			page: 0,
+			total_pages: 0,
+			total_results: 0
+		});
+		loadMoreStatus.value = null;
+	};
+
+	const refresherTriggered = ref<boolean>(false);
+	const onRefresherrefresh = async () => {
+		refresherTriggered.value = true;
+		const { page: currentPage, results, total_pages } = (await apis.getMovies('top_rated', 1)) as types.MovieList;
+		movieList.value.page = currentPage;
+		movieList.value.results = results;
+		refresherTriggered.value = false;
+		loadMoreStatus.value = currentPage === total_pages ? 'no-more' : 'more';
+	};
+
+	const onMovieDetail = (id : number) => {
+		uni.navigateTo({
+			url: `../../views/movieDetail/index?id=${id}`
+		});
+	};
+
+
+	const onScroll2Lower = () => {
+		if (loadMoreStatus.value === 'more') {
+		}
+	};
 </script>
 
 <style lang="scss" scoped>
 	.container {
 		padding: 20rpx;
 
-		.slogan {
-			font-size: 36rpx;
-			line-height: 48rpx;
-		}
+		.movie-list {
+			display: flex;
+			justify-content: space-evenly;
+			gap: 30rpx;
+			flex-wrap: wrap;
 
-		.search {
-			margin: 20rpx 0 30rpx;
-			border-radius: 999px;
-			overflow: hidden;
+			.movie-item {
+				.poster {
+					width: 180rpx;
+					height: 270rpx;
+					border-radius: 15rpx;
+					overflow: hidden;
+					flex-shrink: 0;
+					position: relative;
+
+					.image {
+						width: 100%;
+						height: 100%;
+					}
+				}
+
+				.vote-average {
+					position: absolute;
+					top: 0;
+					right: 0;
+					color: gold;
+					font-size: 24rpx;
+					background-color: #333;
+					padding: 4rpx 15rpx;
+					border-radius: 0 0 0 15rpx;
+				}
+
+				.title {
+					background-color: #333;
+					color: #FFF;
+					position: absolute;
+					width: 100%;
+					text-align: center;
+					bottom: 5rpx;
+					left: 0;
+					font-size: 24rpx;
+					white-space: nowrap;
+					overflow: hidden;
+					text-overflow: ellipsis;
+				}
+			}
 		}
 	}
 </style>
