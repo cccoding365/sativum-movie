4331962bc8f67791680836f3a845103397c567a4
DoubledConG <992196871@qq.com>
2023-12-21 22:45:05 +0800

    feat: add i18n

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
-import App from './App'
-import { createSSRApp } from 'vue'
-export function createApp() {
-	const app = createSSRApp(App)
-	return {
-		app
+import { createSSRApp } from 'vue';
+import { createI18n } from 'vue-i18n';
+import App from './App';
+
+// import zhCN from '@/locales/zh-CN';
+// import enUS from '@/locales/en-US';
+
+let i18nConfig = {
+	locale: uni.getLocale(),
+	messages: {
+
 	}
+};
+
+const i18n = createI18n(i18nConfig);
+
+export function createApp() {
+	const app = createSSRApp(App);
+	return {
+		app,
+		i18n
+	};
 }
