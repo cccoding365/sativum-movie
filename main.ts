13f3362ea17b9176b0fcb03b3e28e90a911fabb8
DoubledConG <992196871@qq.com>
2023-12-28 21:56:33 +0800

    feat: delete the useless files

--- main.ts
+++ main.ts
@@ -1,23 +1,9 @@
 import { createSSRApp } from 'vue';
-import { createI18n } from 'vue-i18n';
-import App from './App';
+import App from '@/App';
 
-// import zhCN from '@/locales/zh-CN';
-// import enUS from '@/locales/en-US';
-
-let i18nConfig = {
-	locale: uni.getLocale(),
-	messages: {
-
-	}
-};
-
-const i18n = createI18n(i18nConfig);
-
-export function createApp() {
+export const createApp = () => {
 	const app = createSSRApp(App);
 	return {
-		app,
-		i18n
+		app
 	};
-}
+};
--- vite.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'vite';
-import uni from '@dcloudio/vite-plugin-uni';
-
-export default defineConfig({
-	plugins: [uni()],
-});
